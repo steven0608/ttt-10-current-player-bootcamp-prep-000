9e054c5812191712bc2c931448497f34683f3f06
Steven Chen <ysteven.chen2@gmail.com>
2018-04-13 05:35:22 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,10 @@
-=begin def turn_count(board)
+def turn_count(board)
 counter=0
 while counter<board.length
   counter+=1
 end
-puts counter
 end
-=end
+
 def current_player(board)
 turn_count(board) % 2 == 0 ? "O" : "X"
 end
